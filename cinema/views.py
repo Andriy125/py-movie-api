5abb895c3b2d83f5602027e5fae4ca3827063c71
Andriy125 <yaruta.andriy@gmail.com>
2026-02-05 22:55:25 +0200

    "Fix movie_detail delete logic, enforce raise_exception validation, add .gitignore"

--- cinema/views.py
+++ cinema/views.py
@@ -14,7 +14,7 @@ def movie_list(request):
 
     elif request.method == 'POST':
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -28,9 +28,9 @@ def movie_detail(request, pk):
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'PUT':
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
+    elif request.method == 'DELETE':
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
